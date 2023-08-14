1ffeb9f9b6f6b10b388631c6d0fd91f5bff2befe
Alvin <darkwell463@gmail.com>
2023-08-14 08:36:48 +0300

    Решение задач: № 8

--- tasks/task_8.py
+++ tasks/task_8.py
@@ -1,1 +1,18 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+onset: int = int(input("Введите начало диапазона: "))
+end: int = int(input("Введите конец диапазона: "))
+
+if onset < end:
+    for x in range(onset, end + 1):
+        if x % 2 == 0:
+            print(x)
+
+elif onset == end:
+    if onset and end % 2 != 0:
+        print(0)
+    else:
+        print(onset or end)
+
+else:
+    for i in range(end, onset + 1):
+        if i % 2 == 0:
+            print(i)
