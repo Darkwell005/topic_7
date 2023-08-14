373fdcf918a6281e66b5ba23a1606a028f2ef5b1
Alvin <darkwell463@gmail.com>
2023-08-14 08:02:13 +0300

    Решение задач: № 2

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,9 +1,12 @@
-print("Таблица умножения для числа 5 с помощью цикла for:")
+print("Таблица умножения с помощью цикла for:")
 num = int(input("Введите число: "))
 
 for x in range(1, 11):
-    print(x * num)  # print(var_1,  , var_2,  , x * num)
+    print(num, "*", x, "=", x * num)
 
-# TODO:
-#  - print('* / =')
-#  - var = 7; print(var)  # 7
+print("Таблица умножения с помощью цикла while:")
+num = int(input("Введите число: "))
+num_2 = 1
+while 0 < num_2 < 11:
+    print(num, "*", num_2, "=", num * num_2)
+    num_2 += 1
