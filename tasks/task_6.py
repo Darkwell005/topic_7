02ee70329efed27daa8823fcccc16ec6cc79f7c9
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-08-16 13:06:36 +0100

    Разбор ДЗ | 7. Циклы
    - Правильно: №8
    - Исправлены: №6, №8
    - На доработку: №7, №9

--- tasks/task_6.py
+++ tasks/task_6.py
@@ -1,1 +1,55 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+num: int = abs(int(input()))
+
+i: int = 0 if num > 0 else 1
+while num > 0:
+    last_digit = num % 10
+    num //= 10  # num = num // 10
+    i += 1
+
+print('Количество цифр в числе:', i)
+
+# -----------------------
+
+# num = 123
+#
+# # %
+# # //
+# # --------------------
+#
+# print(num)
+# last = num % 10
+# print(last)
+#
+# print('---------------------')
+# num = num // 10
+# print(num)
+#
+# last = num % 10
+# print(last)
+#
+# print('---------------------')
+# num = num // 10
+# print(num)
+#
+# last = num % 10
+# print(last)
+
+# -------------------------
+# num = int(input())
+#
+# num_copy = num
+# prod = 1
+# i = 1
+# while num > 0:
+#     last_digit = num % 10
+#     prod *= last_digit
+#     print('Последняя цифра в', i, 'итерации', last_digit)
+#     num = num // 10
+#     print('Отбросили цифру', num)
+#     print('------------\n')
+#
+#     i += 1
+#
+# print(i)
+# print(prod)
+# print(num_copy, num)
--- tasks/task_8.py
+++ tasks/task_8.py
@@ -1,21 +1,11 @@
 onset: int = int(input("Введите начало диапазона: "))
 end: int = int(input("Введите конец диапазона: "))
 
-if onset < end:
-    for x in range(onset, end + 1):
-        if x % 2 == 0:
-            print(x)
+if onset > end:
+    onset, end = end, onset
 
-elif onset == end:
-    if (onset and end) % 2 != 0:  # здесь не понимаю
+for i in range(onset, end + 1):
+    if i % 2 == 0:
+        print(i)
+    elif onset == end and i % 2 != 0:
         print(0)
-    else:
-        print(onset or end)  # здесь не понимаю
-
-else:
-    for i in range(end, onset + 1):
-        if i % 2 == 0:
-            print(i)
-
-# Также в Вашем коде повторение кода, это с 4 по 7 и с 15 по 18 строки
-# Решение этой задачи может быть улучшено!
