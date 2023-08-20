83d9c06ab3704475cedf0bc3302e70be1c23b5ea
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-08-20 12:45:57 +0100

    Разбор ДЗ | 7. Циклы
    - Правильно:
    - Неправильно: №9
    - Исправлены: №7, №9

--- tasks/task_7.py
+++ tasks/task_7.py
@@ -1,1 +1,10 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+n: int = int(input("Введите число: "))
+
+largest_prime_num: int = 2
+
+while largest_prime_num <= n:
+    if n % largest_prime_num != 0:
+        largest_prime_num += 1
+    else:
+        n //= largest_prime_num
+        print(largest_prime_num, end=" ")
