73d18300cff85a6fd1e3c2f9a629ddf8c1477269
Alvin <darkwell463@gmail.com>
2023-08-23 14:08:38 +0300

    Решение задач: № 11

--- tasks/task_11.py
+++ tasks/task_11.py
@@ -1,1 +1,15 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+num: int = int(input("Введите целое положительное число: "))
+
+x = 0
+while x < num:
+    i = 0
+    x += 1
+    while i < x:
+        print(x, end=" ")
+        i += 1
+    print()
+
+# for i in range(1, num + 1):
+#     for j in range(i):
+#         print(i, end=" ")
+#     print()
