fab92ac6ea2d894cae6372bf9c167d26d718974d
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-08-28 14:51:11 +0100

    Проверка ДЗ | 7. Циклы
    - Правильно: №13,
    
    Добавил своё решение

--- tasks/task_13.py
+++ tasks/task_13.py
@@ -1,11 +1,13 @@
 num: int = int(input("Введите целое положительное число: "))
-#
-# counter: int = 1
-# for i in range(1, num + 1):
-#     for j in range(i):
-#         print(counter, end=" ")
-#         counter += 1
-#     print()
+
+counter: int = 1
+for i in range(1, num + 1):
+    for j in range(i):
+        print(counter, end=" ")
+        counter += 1
+    print()
+
+# -------------------------- while
 counter: int = 1
 i: int = 0
 while i < num:
@@ -15,4 +17,12 @@ while i < num:
         print(counter, end=" ")
         j += 1
         counter += 1
+    print()
+
+# ------------------ ¯\_(ツ)_/¯
+for i in range(1, num + 1):
+    current_number: int = i * (i - 1) // 2 + 1  # I Love Math
+    for j in range(i):
+        print(current_number, end=" ")
+        current_number += 1
     print()
