499a7c3460939a6f74ee75a53209c0bde8131f19
Alvin <darkwell463@gmail.com>
2023-08-28 11:24:17 +0300

    Решение задач: № 13

--- tasks/task_13.py
+++ tasks/task_13.py
@@ -1,8 +1,18 @@
 num: int = int(input("Введите целое положительное число: "))
-
+#
+# counter: int = 1
+# for i in range(1, num + 1):
+#     for j in range(i):
+#         print(counter, end=" ")
+#         counter += 1
+#     print()
 counter: int = 1
-for i in range(1, num + 1):
-    for j in range(i):
+i: int = 0
+while i < num:
+    i += 1
+    j: int = 0
+    while j < i:
         print(counter, end=" ")
+        j += 1
         counter += 1
     print()
