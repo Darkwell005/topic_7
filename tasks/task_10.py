e63bd414caf669d7a8fabb6b5fe4fb13a5bd4bda
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-08-30 12:39:19 +0100

    Проверка ДЗ | 7. Циклы
    - Правильно: №10, №14
    - Неправильно:
    - Улучшены: №10, №14
    - На доработку: №14, №15
    
    Добавил своё решение

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -3,20 +3,24 @@ end: int = int(input("Введите конец диапазона: "))
 
 if onset == end or (onset < 0 or end < 0):
     print("Некорректный диапазон")
-
 else:
-    flag = False
+    flag: bool = False
     # if onset > end:
     #     onset, end = end, onset
+    #
+    """
+    onset, end = (находим_минмальное_число_из_двух, 
+                  находим_максимальное_число_из_двух)
+    """
 
-    onset, end = max()
+    onset, end = min(onset, end), max(onset, end)
     for num in range(onset, end + 1):
 
-        num_digits = len(str(num))
-        sum_of_digits = 0
+        num_digits: int = len(str(num))
+        sum_of_digits: int = 0
         temp_num = num
         while temp_num > 0:
-            digit = temp_num % 10
+            digit: int = temp_num % 10
             sum_of_digits += digit ** num_digits
             temp_num //= 10
 
