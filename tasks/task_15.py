89f2f02f2a4e492bf304a0908e5ec0e6d87f387e
Alvin <darkwell463@gmail.com>
2023-08-30 15:27:11 +0300

    Решение задач: № 15

--- tasks/task_15.py
+++ tasks/task_15.py
@@ -1,24 +1,19 @@
 num: int = int(input("Введите целое положительное число: "))
 
-for i in range(num):
+for i in range(num - 1):
     for j in range(num - i - 1):
-        print(' ', end=' ')
+        print(end=' ')
 
     for k in range(2 * i + 1):
-        print('*', end=' ')
+        print('*', end='')
     print()
 
 # ----------------
 
-i = 0
-while i < num:
-    j = 0
-    while j < num - i - 1:
-        print(' ', end=' ')
-        j += 1
-    k = 0
-    while k < 2 * i + 1:
-        print('*', end=' ')
-        k += 1
+for i in range(num - 1, -1, -1):
+    for j in range(num - i - 1):
+        print(end=" ")
+    for k in range(2 * i + 1):
+        print("*", end="")
     print()
-    i += 1
+
