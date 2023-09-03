7fe716660d82e88a5113900b007d3d5ed6ab5a8c
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-09-03 12:44:59 +0100

    Проверка ДЗ | 7. Циклы
    - Правильно: №15
    - На доработку: №14

--- tasks/task_15.py
+++ tasks/task_15.py
@@ -1,20 +1,20 @@
 num: int = int(input("Введите целое положительное число: "))
 
-# for i in range(num - 1):
-#     for j in range(num - i - 1):
-#         print(end=' ')
-#     for k in range(2 * i + 1):
-#         print('*', end='')  # Придерживайтесь одному стилю,
-#         # либо одинарные,
-#         # либо двойные кавычки в программе
-#     print()
-#
-# for i in range(num - 1, -1, -1):
-#     for j in range(num - i - 1):
-#         print(end=" ")
-#     for k in range(2 * i + 1):
-#         print("*", end="")
-#     print()
+for i in range(num - 1):
+    for j in range(num - i - 1):
+        print(end=' ')
+    for k in range(2 * i + 1):
+        print('*', end='')  # Придерживайтесь одному стилю,
+        # либо одинарные,
+        # либо двойные кавычки в программе
+    print()
+
+for i in range(num - 1, -1, -1):
+    for j in range(num - i - 1):
+        print(end=" ")
+    for k in range(2 * i + 1):
+        print("*", end="")
+    print()
 # -----------------------------------
 
 i = 0
@@ -30,8 +30,8 @@ while i < num - 1:
     print()
     i += 1
 
-i = 0
-while i < num:
+i = num - 1
+while i > -1:
     j = 0
     while j < num - i - 1:
         print(end=' ')
@@ -42,9 +42,3 @@ while i < num:
         k += 1
     print()
     i -= 1
-
-
-
-
-
-
