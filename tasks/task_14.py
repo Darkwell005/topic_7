b96270a13e9d463f4308395891bb5c8166aab676
Alvin <darkwell463@gmail.com>
2023-09-04 14:06:30 +0300

    Решение задач: № 14

--- tasks/task_14.py
+++ tasks/task_14.py
@@ -1,19 +1,33 @@
 num: int = int(input("Введите целое положительное число: "))
-
-for i in range(num - 1, 0, -1):
-    for j in range(num - i - 1):
-        print(end=" ")
-    for k in range(2 * i + 1):
-        print("*", end="")
-    print()
-
-for i in range(num):
-    for j in range(num - i - 1):
-        print(end=" ")
-    for k in range(2 * i + 1):
-        print("*", end="")
-    print()
+#
+# for i in range(num - 1, 0, -1):
+#     for j in range(num - i - 1):
+#         print(end=" ")
+#     for k in range(2 * i + 1):
+#         print("*", end="")
+#     print()
+#
+# for i in range(num):
+#     for j in range(num - i - 1):
+#         print(end=" ")
+#     for k in range(2 * i + 1):
+#         print("*", end="")
+#     print()
 # ----------------
+
+i = num - 1
+while i > 0:
+    j = 0
+    while j < num - i - 1:
+        print(end=" ")
+        j += 1
+    k = 0
+    while k < 2 * i + 1:
+        print("*", end="")
+        k += 1
+    print()
+    i -= 1
+
 
 i = 0
 while i < num:
@@ -28,44 +42,4 @@ while i < num:
     print()
     i += 1
 
-program_result = """
-*****************
- ***************
-  *************
-   ***********
-    *********
-     *******
-      *****
-       ***
-        *
-       ***
-      *****
-     *******
-    *********
-   ***********
-  *************
- ***************
-*****************
-"""
 
-test_result = """
-*****************
- ***************
-  *************
-   ***********
-    *********
-     *******
-      *****
-       ***
-        *
-       ***
-      *****
-     *******
-    *********
-   ***********
-  *************
- ***************
-*****************
-"""
-
-print(program_result == test_result)
